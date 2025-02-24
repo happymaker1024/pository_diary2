cc00d330b45f8379a05922b817c864f8d93753f2
happymaker <happymaker1024@gmail.com>
2025-02-24 17:00:09 +0900

    부트스트랩 적용과 예외처리

--- main.js
+++ main.js
@@ -8,17 +8,32 @@ let diary=""
 // 함수 정의
 function saveHandler(){
     // 사용자가 웹페이지에서 입력한 값을 메모리에 저장
-    let date = $("#date").val()
-    let text = $("#text").val()
-    let mood = $("#mood").val()
+    let date = $("#date").val();
+    let text = $("#text").val();
+    let mood = $("#mood").val();
+
+    // 예외처리
+    if (date == "") {
+        alert("날짜를 선택하세요.")
+        return false
+    }
+    if (text == "") {
+        alert("긍정확언을 입력하세요.")
+        return false
+    }
+
     // 입력한 값을 한문장으로 만들기
     diary = `<p>${date}: ${text} = ${mood}</p>` + diary
     // console.log(diary)
     $("#result").html(diary)
 
     localStorage.setItem("diary", diary);
-    let storage1 = localStorage.getItem("diary");
-    console.log(storage1)
+    // let storage1 = localStorage.getItem("diary");
+    // console.log(storage1)
+
+    $("#date").val("");
+    $("#text").val("");
+
 }
 // 스토리지 테스트
 // localStorage.setItem("test-key", "테스트입니다");
@@ -29,6 +44,11 @@ function saveHandler(){
 $(document).ready(function() {
     // 문서가 로딩되면 로컬 저장소에 있는 긍정확언 불러오기
     diary = localStorage.getItem("diary");
+    
+    if (diary == null) {
+        diary = "";
+    }
+
     // id가 save로 정의한 요소를 클릭했을때, saveHandler 호출
-    $("#save").click(saveHandler)
+    $("#save").click(saveHandler);
 })
